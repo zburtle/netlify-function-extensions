508b260d3a3677e038ccebb266ee02f35ded75ef
Zach Burtle <zach@burtlewebservices.com>
2021-12-22 00:48:16 -0600

    remove logging

--- api/admin-functions.ts
+++ api/admin-functions.ts
@@ -16,14 +16,7 @@ export class AdminFunctions {
     }
 
     async getUserById(userId: string, token: string): Promise<User> {
-        console.log(this.netlifyIdentityUrl);
-        console.log(this.usersUrl);
-        console.log(userId);
-
         var getUserByIdUrl = urljoin(this.netlifyIdentityUrl, this.usersUrl, userId);
-
-        console.log(getUserByIdUrl);
-
         var result = await axios.get<User>(getUserByIdUrl, { headers: { Authorization: `Bearer ${token}` }});
 
         return result.data;
