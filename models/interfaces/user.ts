7d51d1c104efc3a558068f9155c6af363df83876
Zach Burtle <zach@burtlewebservices.com>
2021-12-22 21:27:18 -0600

    Add roles to app metadata

--- models/interfaces/user.ts
+++ models/interfaces/user.ts
@@ -1,11 +1,14 @@
+import { AppMetaData } from "./app-metadata";
+import { UserMetaData } from "./user-metadata";
+
 export interface User {
     id: string;
     aud: string;
     role: string;
     email: string;
     confirmation_sent_at: Date;
-    app_metadata: any,
-    user_metadata: any,
+    app_metadata: AppMetaData,
+    user_metadata: UserMetaData,
     created_at: Date;
     updated_at: Date;
 }
