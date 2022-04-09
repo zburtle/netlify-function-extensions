de8df84376702c93b05e1a1bd5d799167048947e
Zach Burtle <zach@burtlewebservices.com>
2022-04-08 22:25:22 -0500

    Export necessary files

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
 export { FunctionInstance } from './function-instance';
 export { AdminFunctions } from './admin-functions';
 export { UserFunctions } from './user-functions';
+export { User } from './models/interfaces/user';
+export { AppMetaData } from './models/interfaces/app-metadata';
+export { UserMetaData } from './models/interfaces/user-metadata';
+export { Users } from './models/interfaces/users';
+export { UserData } from './models/interfaces/user-data';
