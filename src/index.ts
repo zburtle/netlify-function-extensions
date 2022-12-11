97cf767396f9f7dd3f6cef23db2404d45a2e4b92
Zach Burtle <zach@burtlewebservices.com>
2022-12-11 17:42:15 -0600

    Fix

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export { NetlifyHelper } from './netlify-helper';
-export { AdminIdentityFunctions as AdminFunctions } from './admin-identity-functions';
-export { UserIdentityFunctions as UserFunctions } from './user-identity-functions';
+export { AdminIdentityFunctions } from './admin-identity-functions';
+export { UserIdentityFunctions } from './user-identity-functions';
 export { GoTrueNodeUser } from './models/interfaces/go-true-node-user';
 export { AppMetaData } from './models/interfaces/app-metadata';
 export { UserMetaData } from './models/interfaces/user-metadata';
