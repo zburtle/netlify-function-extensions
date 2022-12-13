a117180f4cca4a73e152288e14baf7348404c0bb
Zach Burtle <zach@burtlewebservices.com>
2022-12-12 20:09:09 -0600

    Remove dependency for gotrue-js

--- src/models/interfaces/go-true-node-user.ts
+++ src/models/interfaces/go-true-node-user.ts
@@ -1,19 +1,26 @@
-import { Token, User } from "gotrue-js";
 import { AppMetaData } from "./app-metadata";
 import { UserMetaData } from "./user-metadata";
 
-export class GoTrueNodeUser<T = AppMetaData, U = UserMetaData> extends User {
-    app_metadata!: T;
-    aud!: string;
-    audience!: string;
-    confirmed_at!: string;
-    created_at!: string;
-    email!: string;
-    id!: string;
-    role!: string;
-    sub!: string;
-    token!: Token;
-    updated_at!: string;
-    url!: string;
-    user_metadata!: U;
+export interface GoTrueNodeUser<T = AppMetaData, U = UserMetaData> {
+    app_metadata: T;
+    aud: string;
+    audience: string;
+    confirmed_at: string;
+    created_at: string;
+    email: string;
+    id: string;
+    role: string;
+    sub: string;
+    token: Token;
+    updated_at: string;
+    url: string;
+    user_metadata: U;
+}
+
+export interface Token {
+    access_token: string;
+    expires_at: number;
+    expires_in: number;
+    refresh_token: string;
+    token_type: 'bearer';
 }
