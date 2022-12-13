4639bca814c828f855d2d8d75b8428a10a81a586
Zach Burtle <zach@burtlewebservices.com>
2022-12-12 20:14:16 -0600

    Laptop changes

--- src/index.ts
+++ src/index.ts
@@ -6,3 +6,4 @@ export { AppMetaData } from './models/interfaces/app-metadata';
 export { UserMetaData } from './models/interfaces/user-metadata';
 export { Users } from './models/interfaces/users';
 export { UserData } from './models/interfaces/user-data';
+export { NetlifyResult } from './models/interfaces/netlify-result';
