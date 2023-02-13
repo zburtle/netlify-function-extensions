1d547380e3a38a0856b10ef529e9bd3c1688cc50
Zach Burtle <zach@burtlewebservices.com>
2023-02-13 17:04:04 -0600

    Rename

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-export { NetlifyFunctionExtensions as NetlifyHelper } from './netlify-function-extensions';
+export { NetlifyFunctionExtensions } from './netlify-function-extensions';
 export { AdminIdentityFunctions } from './admin-identity-functions';
 export { UserIdentityFunctions } from './user-identity-functions';
-export { NetlifyIdentityUser as GoTrueNodeUser } from './models/interfaces/netlify-identity-user';
-export { AppMetadata as AppMetaData } from './models/interfaces/app-metadata';
-export { UserMetadata as UserMetaData } from './models/interfaces/user-metadata';
+export { NetlifyIdentityUser } from './models/interfaces/netlify-identity-user';
+export { AppMetadata } from './models/interfaces/app-metadata';
+export { UserMetadata } from './models/interfaces/user-metadata';
 export { Users } from './models/interfaces/users';
 export { UserData } from './models/interfaces/user-data';
 export { NetlifyResult } from './models/interfaces/netlify-result';
